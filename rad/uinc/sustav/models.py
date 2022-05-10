81bc13f6132e5eb76854a6a29fb6c098c88bee9d
matija <matija.juric@hotmail.com>
2022-05-10 23:20:03 +0200

    sustav - home page(1)

--- rad/uinc/sustav/models.py
+++ rad/uinc/sustav/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from taggit.managers import TaggableManager
+
+class Incident(models.Model):
+    name = models.CharField(max_length=100)
+    spotted_by = models.CharField(max_length=30)
+    # date_spotted = models.DateTimeField('date spotted')
+    # severity = models.NESTO
+    tags = TaggableManager() # mozda nakaciti severity na ovo
+
+    def __str__(self) -> str:
+        rv = self.name + '\n'
+        rv += 'Spotted by: ' + self.spotted_by + '\n'
+        # rv += self.date_spotted
+        return rv
+    
+
