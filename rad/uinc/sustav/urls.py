a77a13e6f5b2bf0b1bf7002bce65cbfdfdcfacc7
matija <matija.juric@hotmail.com>
2022-05-24 00:00:55 +0200

    sustav - success page after adding new incident

--- rad/uinc/sustav/urls.py
+++ rad/uinc/sustav/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('statistics/', views.statistics, name='statistics'),
     path('add_new/', views.IncidentCreateView.as_view(), name='add_new'),
+    path('add_new/success/', views.success, name='success')
 ]
--- rad/uinc/sustav/views.py
+++ rad/uinc/sustav/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -18,11 +18,18 @@ def statistics(request):
     context = {'number_of_incidents': number_of_incidents}
     return render(request, 'statistics.html', context)
 
+def success(request):
+    return render(request, 'success.html')
+
+def incident_details(request):
+    return render(request, '')
+
 class IncidentCreateView(CreateView):
     model = Incident
     fields = ('name', 'spotted_by', 'date_spotted', 'severity')
+    success_url = 'success/'
 
     def form_valid(self, form) -> HttpResponse:
         form.save()
         # print(form)
-        return HttpResponse('success')
+        return super().form_valid(form)
