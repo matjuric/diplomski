27d2162f955161ee1a63bbf2354ac1b4e310a6ec
matija <matija.juric@hotmail.com>
2022-05-27 15:27:29 +0200

    sustav - logged-in shows username and user group in navbar

--- rad/uinc/sustav/models.py
+++ rad/uinc/sustav/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
-from taggit.managers import TaggableManager
-
 class Incident(models.Model):
     name = models.CharField(max_length=100)
     spotted_by = models.CharField(max_length=30)
     date_spotted = models.DateTimeField('date spotted', null=True)
+
     SEVERITIES = (
         ('L', 'low'),
         ('M', 'medium'),
         ('H', 'high')
     )
     severity = models.CharField(max_length=1,choices=SEVERITIES, default='low')
-    # severity = models.NESTO
-    # tags = TaggableManager() # mozda nakaciti severity na ovo
+
+    RESOLVED_OPTIONS = (
+        ('Y', 'yes'),
+        ('N', 'no')
+    )
+    resolved = models.CharField(max_length=1, choices=RESOLVED_OPTIONS, default='no')
 
     def __str__(self) -> str:
         rv = self.name + '\n'
         rv += 'Spotted by: ' + self.spotted_by + '\n'
         rv += str(self.date_spotted) if self.date_spotted else ''
         rv += ' SEVERITY: ' + self.severity + '\n'
+        rv += ' RESOLVED: ' + self.resolved + '\n'
         rv += ' private_key = \n' + str(self.pk)
         return rv
     
